4bcac133084ceeeb002bc36a8d3513896fe988a4
FraiMing <1951126494@qq.com>
2025-09-28 18:45:57 +0800

    secondCommit

---
+++ fuliming/复数/test.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type complex struct {
+	real float64
+	fade float64
+}
+
+func newComplex(Real, Fade float64) *complex {
+	return &complex{real: Real, fade: Fade}
+}
+
+func (one *complex) Add(another *complex) *complex {
+	return &complex{
+		real: one.real + another.real,
+		fade: one.fade + another.fade,
+	}
+}
+
+func (one *complex) Sub(another *complex) *complex {
+	return &complex{
+		real: one.real - another.real,
+		fade: one.fade - another.fade,
+	}
+}
+
+func (one *complex) Mul(another *complex) *complex {
+	return &complex{
+		real: one.real * another.real,
+		fade: one.fade * another.fade,
+	}
+}
+
+func (one *complex) Div(another *complex) *complex {
+	return &complex{
+		real: one.real / another.real,
+		fade: one.fade / another.fade,
+	}
+}
+
+func (one *complex) Abs() float64 {
+	return math.Sqrt(one.real * one.real + one.fade * one.fade)
+}
+
+
+func (one *complex) ToString() string {
+	if one.fade == 0 {
+		return fmt.Sprintf("%g", one.real)
+	}
+
+	if one.real == 0 {
+		return fmt.Sprintf("%gi", one.fade)
+	}
+	
+	if one.fade > 0 {
+		return fmt.Sprintf("%g+%gi", one.real, one.fade)
+	}
+	return fmt.Sprintf("%g%gi", one.real, one.fade) 
+}
+
+func main() {
+
+	comPlex1 := newComplex(1, 2)
+	comPlex2 := newComplex(3, 4)
+
+	fmt.Printf("comPlex1 = %s\n", comPlex1.ToString())
+	fmt.Printf("comPlex2 = %s\n", comPlex2.ToString())
+	fmt.Printf("模长 |comPlex1| = %.2f\n", comPlex1.Abs())
+
+    sum := comPlex1.Add(comPlex2)
+	fmt.Printf("加法: %s + %s = %s\n", comPlex1.ToString(), comPlex2.ToString(), sum.ToString())
+	
+	
+	diff := comPlex1.Sub(comPlex2)
+	fmt.Printf("减法: %s - %s = %s\n", comPlex1.ToString(), comPlex2.ToString(), diff.ToString())
+	
+	
+	product := comPlex1.Mul(comPlex2)
+	fmt.Printf("乘法: %s × %s = %s\n", comPlex1.ToString(), comPlex2.ToString(), product.ToString())
+	
+
+	quotient := comPlex1.Div(comPlex2)
+	fmt.Printf("除法: %s / %s = %s\n", comPlex1.ToString(), comPlex2.ToString(), quotient.ToString())
+}
